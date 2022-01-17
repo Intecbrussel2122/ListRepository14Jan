52cbe0bad06ab4f91dc8042d2263c600f099f8c8
Kenan Kurda <intecbrussel2122@gmail.com>
2022-01-17 13:19:13 +0100

    TV class complited

--- ListRepository/Models/TV.cs
+++ ListRepository/Models/TV.cs
@@ -1,4 +1,5 @@
-﻿using ListRepository.Interfaces;
+﻿using ListRepository.Database;
+using ListRepository.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,32 +20,81 @@ namespace ListRepository.Models
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            Data.ProductList.RemoveAt(id - 1);
         }
 
         public bool Find(int find)
         {
-            throw new NotImplementedException();
+            var result = Data.ProductList.Find(f => f.Id == find);
+            if (result != null)
+            {
+                return true;
+            }
+            return false;
         }
 
         public void Insert(ProductBase product)
         {
-            throw new NotImplementedException();
+            Data.ProductList.Add(product);
         }
 
         public List<ProductBase> SelectAll()
         {
-            throw new NotImplementedException();
+            return Data.ProductList;
         }
 
         public ProductBase SelectSingle(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                ProductBase productToReturn = Data.ProductList[id - 1];
+                return productToReturn;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
         public void Update(ProductBase product)
         {
-            throw new NotImplementedException();
+            try
+            {
+                ProductBase updateProduct = SelectSingle(product.Id);
+                if (updateProduct != null)
+                {
+                    //updateProduct.Id = product.Id;//NOOIt veranderen
+                    updateProduct.Name = product.Name;
+                    updateProduct.Price = product.Price;
+                    updateProduct.Category = product.Category;
+                    updateProduct.DateUpdated = DateTime.Now;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void ShowTvs()
+        {
+            Console.WriteLine("List of Tv's in the stock");
+            Console.WriteLine();
+
+            foreach (var item in SelectAll().OfType<TV>())
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in SelectAll())
+            {
+                if (item is TV && item.Name == "Sony")
+                {
+                    Console.WriteLine(item);
+                }
+            }
         }
     }
 }
